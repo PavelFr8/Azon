c53bb3cd181f950cabc01983443e491f3a5709a7
Pavel <151254599+PavelFr8@users.noreply.github.com>
2024-03-19 00:55:28 +0300

    things for host

--- Azon/data/allowes_file.py
+++
@@ -1,2 +0,0 @@
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in {'png', 'jpg', 'jpeg'}
--- start.sh
+++ start.sh
@@ -22,4 +22,4 @@ fi
 $VIRTUALENV/bin/pip install -r requirements.txt
 
 # Run your glorious application
-$VIRTUALENV/bin/python3 Azon/server.py
+$VIRTUALENV/bin/python3 server.py
