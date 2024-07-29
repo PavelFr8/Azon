241930dcc83232ece531546424f1554e85f6e2f1
Pavel <151254599+PavelFr8@users.noreply.github.com>
2024-07-29 03:30:53 +0300

    all system of working with shopping cart changed

--- app/api/azon_api/cart.py
+++ app/api/azon_api/cart.py
@@ -6,6 +6,7 @@ from app.models import ShoppingCart, Item
 from app import db, logger
 
 
+# Add item to the shopping cart
 @module.route("/addToCart/<int:article>", methods=["POST"])
 @login_required
 def api_add_to_cart(article):
@@ -28,4 +29,23 @@ def api_add_to_cart(article):
         logger.error(f"Error add item to shopping cart: {e}")
         db.session.rollback()
         flash("Произошла ошибка при сохранении в корзину", 'danger')
+        return jsonify({"success": False}), 400
+
+
+# Delete from shopping cart
+@module.route("/deleteFromCart/<int:article>", methods=["DELETE"])
+@login_required
+def delete_from_cart(article):
+    try:
+        item = Item.query.filter_by(article=article).first()
+        shopping_cart = ShoppingCart.query.filter(
+            ShoppingCart.user_id == current_user.id,
+            ShoppingCart.item_id == item.id).first_or_404()
+
+        db.session.delete(shopping_cart)
+        db.session.commit()
+        return jsonify({"success": True}), 204
+    except Exception as e:
+        db.session.rollback()
+        logger.error(f"Ошибка при удалении товара из корзины: {e}")
         return jsonify({"success": False}), 400
--- app/modules/shopping_cart/routes.py
+++ app/modules/shopping_cart/routes.py
@@ -1,38 +1,10 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template
 from flask_login import login_required, current_user
 
-import base64
-
-from app.models import ShoppingCart, Item
-from app import db, logger
 from . import module
 
 
-# Добавление товара в корзину
-@module.route('/add/<int:article>', methods=['POST'])
-@login_required
-def add(article):
-    try:
-        item = Item.query.filter_by(article=article).first()
-        shopping_cart: ShoppingCart = ShoppingCart.query.filter(ShoppingCart.user_id == current_user.id,
-                                                                ShoppingCart.item_id == item.id).first()
-        if shopping_cart:
-            shopping_cart.amount += 1
-        else:
-            shopping_cart = ShoppingCart(
-                user_id=current_user.id,
-                item_id=item.id,
-                amount=1
-            )
-        db.session.add(shopping_cart)
-        db.session.commit()
-    except Exception as e:
-        logger.error(f"Error add item to shopping cart: {e}")
-        db.session.rollback()
-        flash("Произошла ошибка при сохранении в корзину", 'danger')
-
-
-# Отображение корзины
+# Show shopping cart page
 @module.route('/')
 @login_required
 def cart():
@@ -41,20 +13,3 @@ def cart():
     for item in items:
         total_price += item.price
     return render_template('shopping_cart/cart.html', title='Корзина', items=items, total_price=total_price)
-
-
-# Удаление товара из корзины
-@module.route('/delete/<int:article>')
-@login_required
-def delete(article):
-    try:
-        item = Item.query.filter_by(article=article).first()
-        shopping_cart: ShoppingCart = ShoppingCart.query.filter(ShoppingCart.user_id == current_user.id,
-                                                                ShoppingCart.item_id == item.id).first()
-        db.session.delete(shopping_cart)
-        db.session.commit()
-        return redirect(url_for('shopping_cart.cart'))
-    except Exception as e:
-        db.session.rollback()
-        logger.error(f"Error delete item from shopping cart: {e}")
-        flash("Произошла ошибка при удалении товара из корзины", 'danger')
